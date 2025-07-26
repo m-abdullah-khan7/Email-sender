edf6416fc7792a2291949c93313890b390051497
m-abdullah-khan7 <muhammadabdullahkhanjt@gmail.com>
2025-07-26 19:34:42 +0500

    The email sending part has been done

--- Emailsender.py
+++ Emailsender.py
@@ -5,7 +5,19 @@ connection.starttls()
 
 sender_email = "abdullahranapakistan@gmail.com"
 sender_pass = "wkeg ohdp ljjl lyck"
-reciver_email = "muhammadabdullahkhan@gmail.com"
+reciver_email = "muhammadabdullahkhanjt@gmail.com"
 
+connection.login(sender_email, sender_pass)
 
+subject = "Sending my first mail through python"
+body = "This is the body of the text and has the message of the text and does not really have anything import and husnain is pretty dumb"
+message = f"Subject: {subject}\n\n{body}"
 print("Email sent")
+
+connection.sendmail(
+    from_addr=sender_email,
+    to_addrs=reciver_email,
+    msg=message
+)
+print("Email was sent")
+connection.quit

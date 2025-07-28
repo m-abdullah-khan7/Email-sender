775727425de44e0194b76083941d651b61625e3e
m-abdullah-khan7 <muhammadabdullahkhanjt@gmail.com>
2025-07-29 04:54:57 +0500

    added logfiles to test

--- Emailsender.py
+++ Emailsender.py
@@ -4,7 +4,7 @@ connection = smtplib.SMTP("smtp.gmail.com", 587)
 connection.starttls()
 
 sender_email = "abdullahranapakistan@gmail.com"
-sender_pass = "wkeg ohdp ljjl lyck"
+sender_pass = "wkeg ohdp ljjl lyck" #you need to add a new one this one will no longer work i removed it
 reciver_email = "muhammadabdullahkhanjt@gmail.com"
 
 connection.login(sender_email, sender_pass)
@@ -12,7 +12,6 @@ connection.login(sender_email, sender_pass)
 subject = "Sending my first mail through python"
 body = "This is the body of the text and has the message of the text and does not really have anything import and husnain is pretty dumb"
 message = f"Subject: {subject}\n\n{body}"
-print("Email sent")
 
 connection.sendmail(
     from_addr=sender_email,

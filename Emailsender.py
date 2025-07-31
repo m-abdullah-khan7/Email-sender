e9ea81e4204a25145f9935f88bf7dff7cef6ff15
m-abdullah-khan7 <muhammadabdullahkhanjt@gmail.com>
2025-07-31 19:54:17 +0500

    Final finished

--- Emailsender.py
+++ Emailsender.py
@@ -1,22 +1,38 @@
 import smtplib
 
-connection = smtplib.SMTP("smtp.gmail.com", 587)
-connection.starttls()
+errors = []
+filename = input("Enter the file name or file path: ")
+line_number = 1
+with open(filename , 'r') as file:
+    lines = file.readlines()
+    for line in lines:
 
-sender_email = "abdullahranapakistan@gmail.com"
-sender_pass = "wkeg ohdp ljjl lyck" #you need to add a new one this one will no longer work i removed it
-reciver_email = "muhammadabdullahkhanjt@gmail.com"
+        if line.find('error') != -1 or line.find('issue') != -1:
+            errors.append(f"Line number {line_number} has the following error: {line}") 
+            errorfound = True
+        line_number += 1    
+body = "".join(errors)
 
-connection.login(sender_email, sender_pass)
 
-subject = "Sending my first mail through python"
-body = "This is the body of the text and has the message of the text and does not really have anything import and husnain is pretty dumb"
-message = f"Subject: {subject}\n\n{body}"
 
-connection.sendmail(
-    from_addr=sender_email,
-    to_addrs=reciver_email,
-    msg=message
-)
-print("Email was sent")
-connection.quit
+if errorfound == True:
+    connection = smtplib.SMTP("smtp.gmail.com", 587)
+    connection.starttls()
+
+    sender_email = "abdullahranapakistan@gmail.com"
+    sender_pass = "gsby rsdo zkmb wzrs" 
+    reciver_email = "muhammadabdullahkhanjt@gmail.com"
+
+    connection.login(sender_email, sender_pass)
+
+    subject = "There is a error in the log file"
+    body = "".join(errors)
+    message = f"Subject: {subject}\n\n{body}"
+
+    connection.sendmail(
+        from_addr=sender_email,
+        to_addrs=reciver_email,
+        msg=message
+    )
+    print("Email was sent")
+    connection.quit
